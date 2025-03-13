c6b4ca191b47182d03f5270e58639279c038e3ab
kohyeonwoo <49010769+kohyeonwoo@users.noreply.github.com>
2025-03-13 17:02:35 +0900

    250313
    기타 파일 제거 및 정리, 새로운 유닛 데이터 추가 및 기능 반영

---
+++ FirstTake/Assets/Scripts/GameScene/Player/Rake.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rake : Player
+{
+    private void Start()
+    {
+        maxHealth = 25.0f;
+        currentHealth = maxHealth;
+    }
+}
---
+++ FirstTake/Assets/Scripts/GameScene/Player/Whale.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Whale : Player
+{
+    private void Start()
+    {
+        maxHealth = 28.0f;
+        currentHealth = maxHealth;
+    }
+}
