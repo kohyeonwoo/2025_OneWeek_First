3b256c8fda271c11b4dfce893c8f86b7f5097f51
kohyeonwoo <49010769+kohyeonwoo@users.noreply.github.com>
2025-03-31 23:09:29 +0900

    250331
    기타 파일 정리 및 플레이어 ai 및 적 ai 관련 버그 오류 수정

--- FirstTake/Assets/Scripts/GameScene/Player/Common/Player.cs
+++ FirstTake/Assets/Scripts/GameScene/Player/Common/Player.cs
@@ -3,28 +3,202 @@ using System.Collections.Generic;
 using UnityEngine.AI;
 using UnityEngine;
 
+public enum CurrentState
+{
+    None = -1, 
+    Idle = 0,
+    Wander,
+    Pursuit,
+    Attack
+}
+
 public class Player : MonoBehaviour, IDamageable
 {
-   // public Transform target;  
-    protected NavMeshAgent agent;
-    public GameObject attackCollision;
-    public GameObject particle;
-    public GameObject deadParticle;
-    public Transform particleLocation;
+    //현재 움직임 상태값 관련 
+    public CurrentState currentState = CurrentState.None;
 
+    //타겟 관련 
+    public Transform target;
+
+    // 컴포넌트 관련
+    public NavMeshAgent nav;
+    public Animator anim;
+    public Rigidbody rigid;
+    public SkinnedMeshRenderer[] meshes;
+
+    //공격 충돌체 관련 
+    public GameObject attackCollision;
+    public GameObject attackCollision2;
+    public GameObject attackCollision3;
+
+    //체력 관련
     protected float maxHealth;
     protected float currentHealth;
 
-    protected Animator anim;
-    protected Rigidbody rigid;
+    //움직임 관련 
+    protected float walkSpeed;
+    protected float runSpeed;
 
-    protected void Init()
+    //추격 관련 
+    [SerializeField]
+    private float targetRange = 5.0f; //타겟 인식 범위
+    [SerializeField]
+    private float chaseLimitRange = 7.0f; //추격 범위
+
+    //공격 수치 관련 
+    [SerializeField]
+    private float attackRange = 3.0f; //==> 공격 범위
+    [SerializeField]
+    private float attackSpeed = 1.0f; //==> 공격 속도
+    public float lastAttackTime = 2.0f; // 공격 주기 계산용 변수
+
+    //플레이어 생사 유무 관련 
+    private bool bDead;
+
+    private void OnEnable()
     {
-        agent = GetComponent<NavMeshAgent>();
-        anim = GetComponent<Animator>();
-        rigid = GetComponent<Rigidbody>();
+        
     }
 
+    private void OnDisable()
+    {
+
+    }
+
+
+    //컴포넌트 / 기타 설정 관련 초기화 부분 
+    public void Init()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
+        meshes = GetComponentsInChildren<SkinnedMeshRenderer>();
+        nav.updateRotation = false;
+    }
+
+    //움직임 상태값 변경 관련 
+    public void ChangeState(CurrentState NewState)
+    {
+        if(currentState == NewState)
+        {
+            return;
+        }
+
+        StopCoroutine(currentState.ToString());
+
+        currentState = NewState;
+
+        StartCoroutine(currentState.ToString());
+    }
+
+    //대기 --> 배회로 자동 상태 변환
+    private IEnumerator AutoChangeFromIdleToWander()
+    {
+        //1~4초 사이에서 무작위로 대기
+        int changeTime = Random.Range(1, 5);
+
+        yield return new WaitForSeconds(changeTime);
+
+        //상태를 배회로 변경
+        ChangeState(CurrentState.Wander);
+    }
+
+    //배회 위치값 계산 
+    private Vector3 CalculateWanderPosition()
+    {
+        float wanderRadius = 10.0f; //현재 위치를 원점으로 하는 원의 반지름
+        int wanderJitter = 0; //선택된 각도 (wanderJitterMin ~ wanderJitterMax)
+        int wanderJitterMin = 0;
+        int wanderJitterMax = 360;
+
+        //현재 캐릭터가 있는 월드의 중심 위치와 크기 (구역을 벗어난 행동을 하지 말도록 하기 위해서)
+        Vector3 rangePosition = Vector3.zero;
+        Vector3 rangeScale = Vector3.one * 100.0f;
+
+        //자신의 위치를 중심으로 반지름(wanderRadius) 거리, 선택된 각도(wanderJitter)에 위치한 좌표를 목표 지점으로 설정
+        wanderJitter = Random.Range(wanderJitterMin, wanderJitterMax);
+        Vector3 targetPosition = transform.position + SetAngle(wanderRadius, wanderJitter);
+
+        //생성된 목표위치가 자신의 구역을 벗어나지 않도록 조절 
+        targetPosition.x = Mathf.Clamp(targetPosition.x, rangePosition.x - rangeScale.x * 0.5f, rangePosition.x + rangeScale.x * 0.5f);
+        targetPosition.y = 0.0f;
+        targetPosition.z = Mathf.Clamp(targetPosition.z, rangePosition.z - rangeScale.z * 0.5f, rangePosition.z + rangeScale.z * 0.5f);
+
+        return targetPosition;
+    }
+
+
+    //각도값 설정 
+    private Vector3 SetAngle(float radius, int angle)
+    {
+        Vector3 position = Vector3.zero;
+
+        position.x = Mathf.Cos(angle) * radius;
+        position.z = Mathf.Sin(angle) * radius;
+
+        return position;
+    }
+
+    //기본 서있는 상태
+
+    private IEnumerator Idle()
+    {
+        //일정 초가 지난 후 배회 상태로 변환되는 코루틴 실행
+        StartCoroutine("AutoChangeFromIdleToWander");
+
+        while(true)
+        {
+            //대기 상황인 경우, 타겟과의 거리에 따라 행동 선택(배회, 추격, 공격)
+           
+
+            yield return null;
+        }
+    }
+
+   
+    private IEnumerator Wander()
+    {
+        float currentTime = 0.0f;
+        float maxTime = 10.0f;
+
+        //이동 속도 설정, 배회 --> 걷는 속도, 추격 --> 뛰는 속도
+        nav.speed = walkSpeed;
+        
+        //목표 위치 설정
+        nav.SetDestination(target.position);
+
+        //목표 위치로 회전
+        Vector3 to = new Vector3(nav.destination.x, 0, nav.destination.z);
+        Vector3 from = new Vector3(transform.position.x, 0, transform.position.z);
+        transform.rotation = Quaternion.LookRotation(to - from);
+
+        anim.SetBool("bMove", true);
+        anim.SetBool("bAttack", false);
+
+        while(true)
+        {
+            currentTime += Time.deltaTime;
+
+            //목표 위치에 근접하거나 배회하는 시간이 많이 길어진다면 
+            to = new Vector3(nav.destination.x, 0, nav.destination.z);
+            from = new Vector3(transform.position.x, 0, transform.position.z);
+
+            if((to - from).sqrMagnitude < 0.01f || currentTime >= maxTime)
+            {
+                //상태 --> 대기로 변경
+                ChangeState(CurrentState.Idle);
+            }
+
+            //타겟과의 거리에 따라 행동을 선택한다(배회, 추격, 공격 )
+
+            yield return null;
+
+        }
+    }
+
+
+    ////////////////////////////////////
+    ///// 공격 충돌체 활성화/비활성화 부분
     public void ActiveAttackCollision()
     {
         attackCollision.SetActive(true);
@@ -35,29 +209,67 @@ public class Player : MonoBehaviour, IDamageable
         attackCollision.SetActive(false);
     }
 
+    public void ActiveAttackCollision2()
+    {
+        attackCollision2.SetActive(true);
+    }
+
+    public void DeActiveAttackCollision2()
+    {
+        attackCollision2.SetActive(false);
+    }
+
+    public void ActiveAttackCollision3()
+    {
+        attackCollision3.SetActive(true);
+    }
+
+    public void DeActiveAttackCollision3()
+    {
+        attackCollision3.SetActive(false);
+    }
+    ////////////////////////////////////
+    ////////////////////////////////////
+
     public void Damage(float Damage)
     {
+
         currentHealth -= Damage;
 
-        AudioManager.Instance.PlaySFX("PlayerHitSound");
-        GameObject obj = Instantiate(particle, particleLocation.position, Quaternion.identity);
-        Destroy(obj, 2.0f);
+        StartCoroutine(ChangeColor());
 
+        AudioManager.Instance.PlaySFX("PlayerHitSound");
+     
         GameManager.Instance.PlayHitEffect();
 
         if (currentHealth <= 0)
         {
             Dead();
         }
+
+    }
+
+    IEnumerator ChangeColor()
+    {
+
+        foreach (SkinnedMeshRenderer mesh in meshes)
+        {
+            mesh.material.color = Color.red;
+        }
+
+        yield return new WaitForSeconds(0.3f);
+
+        foreach (SkinnedMeshRenderer mesh in meshes)
+        {
+            mesh.material.color = Color.white;
+        }
+
     }
 
     public void Dead()
     {
-       // GameObject obj = Instantiate(deadParticle, particleLocation.position, Quaternion.identity);
-       // Destroy(obj, 3.0f);
          this.gameObject.SetActive(false);
-        //Destroy(this.gameObject);
-
+       
         GameManager.Instance.SetActiveEndPanel();
     }
 
--- FirstTake/Assets/Scripts/GameScene/Player/Golem.cs
+++ FirstTake/Assets/Scripts/GameScene/Player/Golem.cs
@@ -5,20 +5,6 @@ using TMPro;
 public class Golem : Player
 {
 
-    [SerializeField]
-    private Transform target;
-
-    [SerializeField]
-    private float range;
-
-    [SerializeField]
-    private float attackRange;
-
-    [SerializeField]
-    private float speed;
-
-    private float distance;
-
     public TextMeshProUGUI golemHealth;
 
     private void Start()
@@ -28,70 +14,8 @@ public class Golem : Player
 
         Init();
     }
-
     private void Update()
     {
-
-        golemHealth.text = currentHealth.ToString();
-
-        UpdateTarget();
-
-        if (target == null)
-        {
-            anim.SetBool("bAttack", false);
-            return;
-        }
-
-        Vector3 direction = target.position - transform.position;
-
-        distance = Vector3.Distance(this.transform.position, target.position);
-
-        Quaternion lookRotation = Quaternion.LookRotation(direction);
-
-        Vector3 rotation = Quaternion.Lerp(this.transform.rotation, lookRotation, Time.deltaTime * 3).eulerAngles;
-        
-        this.transform.rotation = Quaternion.Euler(0.0f, rotation.y, 0.0f);
-
-        float distanceThisFrame = speed * Time.deltaTime;
-
-        if (distance < 2.0f)
-        {
-            anim.SetBool("bAttack", true);
-        }
-
-
+        golemHealth.text = currentHealth.ToString(); 
     }
-
-
-    private void UpdateTarget()
-    {
-        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
-
-        float shortestDistance = Mathf.Infinity;
-
-        GameObject nearestEnemy = null;
-
-        foreach (GameObject enemy in enemies)
-        {
-            float distanceToEnemy = Vector3.Distance(this.transform.position, enemy.transform.position);
-
-            if (distanceToEnemy < shortestDistance)
-            {
-                shortestDistance = distanceToEnemy;
-
-                nearestEnemy = enemy;
-            }
-        }
-
-        if (nearestEnemy != null && shortestDistance <= range)
-        {
-            target = nearestEnemy.transform;
-        }
-        else
-        {
-            target = null;
-        }
-
-    }
-
 }
